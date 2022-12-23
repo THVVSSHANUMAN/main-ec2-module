4f107749f74ba55a071736c26ef63a4cdb768298
THVVSSHANUMAN <119286323+THVVSSHANUMAN@users.noreply.github.com>
2022-12-23 20:05:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,9 +6,10 @@ locals {
   is_t_instance_type = replace(var.instance_type, "/^t(2|3|3a){1}\\..*$/", "1") == "1" ? true : false
 }
 
+/*
 data "aws_ssm_parameter" "this" {
   count = local.create ? 1 : 0
-
+*/
   name = var.ami_ssm_parameter
 }
 
